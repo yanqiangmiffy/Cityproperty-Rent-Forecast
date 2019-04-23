1ff486f32e9da476b27fa3d6bb2387d88b1235e8
yanqiangmiffy <1185918903@qq.com>
2019-04-23 15:06:35 +0800

    提取特征

---
+++ 03_lr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-  
+""" 
+@author:quincyqiang 
+@license: Apache Licence 
+@file: 03_lr.py 
+@time: 2019-04-22 22:12
+@description:
+"""
---
+++ gen_feas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-  
+""" 
+@author:quincyqiang 
+@license: Apache Licence 
+@file: gen_feas.py 
+@time: 2019-04-22 18:26
+@description:
+"""
