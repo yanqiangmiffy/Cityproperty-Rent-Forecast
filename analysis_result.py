a5f91c0e60a3bbe36fc11cadc66c87e182cf9686
yanqiangmiffy <1185918903@qq.com>
2019-04-27 23:15:23 +0800

    根据area/housetype/communityName筛选训练集

--- analysis_result.py
+++ analysis_result.py
@@ -8,8 +8,59 @@
 @description: 结果分析 从交叉验证上面分析误差
 """
 import pandas as pd
-pd.set_option('display.max_columns',100)
-true_pred=pd.read_csv('output/full_df.csv')
-true_pred['error']=abs(true_pred['tradeMoney']-true_pred['pred_tradeMoney'])
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = [u'Microsoft YaHei']
+
+true_pred = pd.read_csv('output/full_df.csv')
+true_pred['error'] = abs(true_pred['tradeMoney'] - true_pred['pred_tradeMoney'])
 # print(true_pred['error'])
-print(true_pred[true_pred['error']>=10000])
+print(true_pred[true_pred['error'] >= 10000])
+
+# 分析训练街和测试集
+## area
+train = pd.read_csv('input/train_data.csv')
+test = pd.read_csv('input/test_a.csv')
+print(train['area'].describe())
+print(test['area'].describe())
+
+# 训练集area最大值15055 最小值为1
+# 测试集area最大值为150 最小值为15
+
+## houseType
+print("houseType")
+print(train['houseType'].value_counts())
+print(test['houseType'].value_counts(), len(test['houseType'].unique()))
+
+test['houseType'].value_counts().plot(kind="bar")
+plt.show()
+
+# houseFloor
+print("houseFloor")
+print(train['houseFloor'].value_counts())
+print(test['houseFloor'].value_counts())
+
+# totalFloor
+print("totalFloor")
+print(train['totalFloor'].describe())
+print(test['totalFloor'].describe())
+
+# train totalFloor 最小值 0 最大值 88
+# test  totalFloor 最小值 2 最大值 53
+
+# communityName
+print("communityName")
+print(train['communityName'].value_counts())
+print(test['communityName'].value_counts())
+
+# region
+print("region")
+print(train['region'].value_counts(), len(train['region'].value_counts()))
+print(test['region'].value_counts(), len(test['region'].value_counts()))
+
+# plate
+print("plate")
+print(train['plate'].value_counts(), len(train['plate'].value_counts()))
+print(test['plate'].value_counts(), len(test['plate'].value_counts()))
--- gen_feas.py
+++ gen_feas.py
@@ -15,11 +15,10 @@ from datetime import datetime
 from itertools import combinations
 from sklearn.cluster import KMeans
 
-# df['tradeTime']=pd.to_datetime(df['tradeTime'])
-# df['interval']=(now-df['tradeTime']).dt.days
-# no_features = ['ID', 'tradeTime', 'tradeMoney',]
 df_train = pd.read_csv('input/train_data.csv')
-print("filter before:", len(df_train))
+df_test = pd.read_csv('input/test_a.csv')
+
+print("filter tradeMoney before:", len(df_train))
 # df_train = df_train.query("tradeMoney>=1000&tradeMoney<15000") # 线下0.87 线上0.86
 # df_train = df_train.query("tradeMoney>=500&tradeMoney<40000")#线下0.8816  线上0.84
 # df_train = df_train.query("tradeMoney>=500&tradeMoney<25000")# 线下 0.8857
@@ -29,7 +28,6 @@ print("filter before:", len(df_train))
 df_train = df_train.query("500<=tradeMoney<20000")  # 线下 lgb_0.876612870005764
 
 print("filter tradeMoney after:", len(df_train))
-# df_train = df_train.query("10<=area<=200")  # 线下 lgb_0.8830538988139025 线上0.867
 df_train = df_train.query("15<=area<=150")  # 线下 lgb_0.8830538988139025 线上0.867
 print("filter area after:", len(df_train))
 
@@ -37,7 +35,25 @@ df_train['area_money']=df_train['tradeMoney']/df_train['area']
 df_train = df_train.query("15<=area_money<300")  # 线下 lgb_0.9003567192921244.csv 线上0.867649
 print("filter area/money after:", len(df_train))
 
-df_test = pd.read_csv('input/test_a.csv')
+
+# totalFloor
+print("filter totalFloor after:", len(df_train))
+df_train = df_train.query("2<=totalFloor<=53")
+print("filter totalFloor after:", len(df_train))
+
+unique_comname = df_test['communityName'].unique()
+print("filter communityName after:", len(df_train))
+df_train = df_train[df_train['communityName'].isin(unique_comname)]
+print("filter communityName after:", len(df_train))
+
+print("houseType")
+
+unique_house = df_test['houseType'].unique()
+print("filter houseType after:", len(df_train))
+df_train = df_train[df_train['houseType'].isin(unique_house)]
+print("filter houseType after:", len(df_train))
+
+
 df = pd.concat([df_train, df_test], sort=False, axis=0, ignore_index=True)
 
 # 数据预处理
@@ -58,22 +74,22 @@ def split_type(x):
 
 df['houseType_shi'], df['houseType_ting'], df['houseType_wei'] = zip(*df['houseType'].apply(lambda x: split_type(x)))
 
-
-def check_type(x):
-    """
-    将房屋类型计数分箱
-    :param x:
-    :return:
-    """
-    if house_type_nums[x] >= 1000:
-        return "high_num"
-    elif 100 <= house_type_nums[x] < 1000:
-        return "median_num"
-    else:
-        return "low_num"
-
-
-df['houseType'] = df['houseType'].apply(lambda x: check_type(x))
+#
+# def check_type(x):
+#     """
+#     将房屋类型计数分箱
+#     :param x:
+#     :return:
+#     """
+#     if house_type_nums[x] >= 1000:
+#         return "high_num"
+#     elif 100 <= house_type_nums[x] < 1000:
+#         return "median_num"
+#     else:
+#         return "low_num"
+#
+#
+# df['houseType'] = df['houseType'].apply(lambda x: check_type(x))
 
 # 交易至今的天数
 now = datetime.now()
@@ -116,15 +132,35 @@ df['now_build_interval'] = 2019 - df['buildYear']
 df['pv'] = df['pv'].fillna(value=int(df['pv'].median()))
 df['uv'] = df['uv'].fillna(value=int(df['uv'].median()))
 
-# 类别特征 具有大小关系编码
-com_categorical_feas = ['rentType', 'houseType', 'houseFloor', 'houseToward', 'houseDecoration']
+# 聚类特征
+# house_cols = ['area', 'rentType', 'houseType_shi', 'houseType_ting', 'houseType_wei', 'houseType', 'houseFloor',
+#               'totalFloor', 'houseToward', 'houseDecoration']
+# km = KMeans(n_clusters=5)
+# km.fit(df[house_cols])
+# df['house_clster'] = km.predict(df[house_cols])
 
-for col in com_categorical_feas:
-    le = LabelEncoder()
-    df[col] = le.fit_transform(df[col])
-    # df[col] = df[col].astype('category')
-categorical_feas=['region', 'plate']
-df=pd.get_dummies(df,columns=categorical_feas)
+life_cols = ['saleSecHouseNum', 'subwayStationNum', 'busStationNum', 'interSchoolNum', 'schoolNum', 'privateSchoolNum',
+             'hospitalNum', 'drugStoreNum', 'gymNum', 'bankNum', 'shopNum', 'parkNum', 'mallNum', 'superMarketNum']
+km = KMeans(n_clusters=3)
+km.fit(df[life_cols])
+df['life_clster'] = km.predict(df[life_cols])
+
+trade_cols = ['totalTradeMoney', 'totalTradeArea', 'tradeMeanPrice', 'tradeSecNum', 'totalNewTradeMoney',
+              'totalNewTradeArea',
+              'tradeNewMeanPrice', 'tradeNewNum']
+km = KMeans(n_clusters=3)
+km.fit(df[trade_cols])
+df['trade_clster'] = km.predict(df[trade_cols])
+
+# 类别特征 具有大小关系编码
+# com_categorical_feas = ['rentType', 'houseType', 'houseFloor', 'houseToward', 'houseDecoration']
+#
+# for col in com_categorical_feas:
+#     le = LabelEncoder()
+#     df[col] = le.fit_transform(df[col])
+#     # df[col] = df[col].astype('category')
+categorical_feas = ['communityName','rentType', 'houseType', 'houseFloor', 'houseToward', 'houseDecoration', 'region', 'plate']
+df = pd.get_dummies(df, columns=categorical_feas)
 # # 添加组合特征
 # relate_pairs1 = ['saleSecHouseNum', 'subwayStationNum', 'busStationNum', 'interSchoolNum', 'schoolNum',
 #                  'privateSchoolNum', 'hospitalNum', 'drugStoreNum', 'gymNum', 'bankNum', 'shopNum', 'parkNum',
@@ -168,45 +204,15 @@ df['stationNum'] = df['subwayStationNum'] + df['busStationNum']
 df['schoolNum'] = df['interSchoolNum'] + df['schoolNum'] + df['privateSchoolNum']
 df['medicalNum'] = df['hospitalNum'] + df['drugStoreNum']
 df['lifeHouseNum'] = df['gymNum'] + df['bankNum'] + df['shopNum'] + df['parkNum'] + df['mallNum'] + df['superMarketNum']
-df['landSupplyTradeRatio'] = df['supplyLandArea'] / df['tradeLandArea']
-# 对数值型的特征，处理为rank特征（鲁棒性好一点）
-# numerical_features=df.loc[:,'saleSecHouseNum':'now_build_interval'].columns.tolist()+['area','totalFloor']
-# for feat in numerical_features:
-#     df[feat] = df[feat].rank() / float(df.shape[0]) # 排序，并且进行归一化
-# rank_feas=['totalTradeMoney']
-# df['totalTradeMoney']=df['totalTradeMoney'].rank()
-# print(df['totalTradeMoney'])
 
 # 重要特征
 df['area_floor_ratio'] = df['area'] / df['totalFloor']
-df['tradeMeanPrice_new_ratio'] = df['tradeMeanPrice'] / df['tradeNewMeanPrice']
-df['tradeMeanPrice_new_sum'] = df['tradeMeanPrice'] + df['tradeNewMeanPrice'] + df['totalTradeMoney']
 df['uv_pv_ratio'] = df['uv'] / df['pv']
 df['uv_pv_sum'] = df['uv'] + df['pv']
 
-# 聚类特征
-house_cols = ['area', 'rentType', 'houseType_shi', 'houseType_ting', 'houseType_wei', 'houseType', 'houseFloor',
-              'totalFloor', 'houseToward', 'houseDecoration']
-km = KMeans(n_clusters=5)
-km.fit(df[house_cols])
-df['house_clster'] = km.predict(df[house_cols])
-
-life_cols = ['saleSecHouseNum', 'subwayStationNum', 'busStationNum', 'interSchoolNum', 'schoolNum', 'privateSchoolNum',
-              'hospitalNum', 'drugStoreNum', 'gymNum', 'bankNum','shopNum','parkNum','mallNum','superMarketNum']
-km = KMeans(n_clusters=3)
-km.fit(df[life_cols])
-df['life_clster'] = km.predict(df[life_cols])
-
-trade_cols=['totalTradeMoney','totalTradeArea','tradeMeanPrice','tradeSecNum','totalNewTradeMoney','totalNewTradeArea',
-            'tradeNewMeanPrice','tradeNewNum']
-km = KMeans(n_clusters=3)
-km.fit(df[trade_cols])
-df['trade_clster'] = km.predict(df[trade_cols])
-
-
 # 特征工程
 no_features = ['ID', 'tradeTime', 'tradeMoney',
-               'buildYear', 'communityName', 'city','area_money'
+               'buildYear', 'communityName', 'city', 'area_money'
                ]
 
 # no_features = no_features + too_many_zeros
