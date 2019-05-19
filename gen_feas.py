f628a67d567dd3840d66302f854037d71dd949d4
yanqiangmiffy <1185918903@qq.com>
2019-05-19 23:35:22 +0800

    筛选误差 5000 线上0.977

--- gen_feas.py
+++ gen_feas.py
@@ -16,12 +16,12 @@ from datetime import datetime
 from itertools import combinations
 from sklearn.cluster import KMeans
 
-# df_train = pd.read_csv('input/train_data.csv')
-# df_test = pd.read_csv('input/test_a.csv')
+df_train = pd.read_csv('input/train_data.csv')
+df_test = pd.read_csv('input/test_a.csv')
 
 # 加载word2vec feat
-df_train = pd.read_csv('input/train_w2v.csv')
-df_test = pd.read_csv('input/test_w2v.csv')
+# df_train = pd.read_csv('input/train_w2v.csv')
+# df_test = pd.read_csv('input/test_w2v.csv')
 # ------------------ 过滤数据 begin ----------------
 print("根据tradeMoney过滤数据:", len(df_train))
 df_train = df_train.query("400<=tradeMoney<25000")  # 线下 lgb_0.876612870005764
@@ -33,24 +33,24 @@ print("filter area after:", len(df_train))
 
 print("根据tradeMoney/area过滤数据:", len(df_train))
 df_train['area_money'] = df_train['tradeMoney'] / df_train['area']
-df_train = df_train.query("20<=area_money<250")  # 线下 lgb_0.9003567192921244.csv 线上0.867649
+df_train = df_train.query("20<=area_money<300")  # 线下 lgb_0.9003567192921244.csv 线上0.867649
 print("filter area/money after:", len(df_train))
 
-# print("根据上次训练的结果，过滤误差较大的数据：")
-# lgb_df = pd.read_csv('output/lgb_df.csv')
-# lgb_df['error']=abs(lgb_df['tradeMoney']-lgb_df['pred_tradeMoney'])
-# lgb_df = lgb_df.query('error<=3000')
-# small_error_ids = lgb_df.ID.values
-# print(len(small_error_ids))
-# df_train = df_train[df_train['ID'].isin(small_error_ids)]
-# print("过滤误差大的训练集之后数据个数：", len(df_train))
+print("根据上次训练的结果，过滤误差较大的数据：")
+lgb_df = pd.read_csv('output/lgb_df.csv')
+lgb_df['error']=abs(lgb_df['tradeMoney']-lgb_df['pred_tradeMoney'])
+lgb_df = lgb_df.query('error<=5000')
+small_error_ids = lgb_df.ID.values
+print(len(small_error_ids))
+df_train = df_train[df_train['ID'].isin(small_error_ids)]
+print("过滤误差大的训练集之后数据个数：", len(df_train))
 
 #
 # totalFloor
 # print("filter totalFloor after:", len(df_train))
 # df_train = df_train.query("2<=totalFloor<=53")
 # print("filter totalFloor after:", len(df_train))
-#
+
 # unique_comname = df_test['communityName'].unique()
 # print("filter communityName after:", len(df_train))
 # df_train = df_train[df_train['communityName'].isin(unique_comname)]
