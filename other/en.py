bc48c7b8984ec72ffdf98d2843e0752156a2a7a6
yanqiangmiffy <1185918903@qq.com>
2019-05-21 18:10:36 +0800

    微软0.86 91开线上0.899

--- other/en.py
+++ other/en.py
@@ -13,6 +13,6 @@ lgb = pd.read_csv('../output/lgb_0.9382180135973881.csv', header=None)
 xgb = pd.read_csv('../output/ebm_0.9027724570728457.csv', header=None)
 
 # res=lgb*0.6+xgb*0.4 # 0.872517
-res = lgb * 0.8 + xgb * 0.2  # 0.877785
+res = lgb * 0.9 + xgb * 0.1 # 0.877785
 # res=lgb*0.8+xgb*0.2   # 0.877785  xgb 原先
-res.to_csv('output/en73.csv', index=None, header=None)
+res.to_csv('../output/en73.csv', index=None, header=None)
