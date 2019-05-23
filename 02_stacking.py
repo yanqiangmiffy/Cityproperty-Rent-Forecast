dae078992121c839be7471259a10c21ea1d00d46
yanqiangmiffy <1185918903@qq.com>
2019-05-24 01:32:54 +0800

    循环遍历

--- 02_stacking.py
+++ 02_stacking.py
@@ -121,6 +121,7 @@ valid_df = pd.DataFrame()
 valid_df['ID'] = valid_id
 valid_df['pred_tradeMoney'] = valid_pred_list
 full_df = pd.merge(raw_df, valid_df, on="ID")
+full_df['error']=full_df['tradeMoney']-full_df['pred_tradeMoney']
 full_df.to_csv('output/lgb_df.csv', index=None)
 
 # ----------xgb------------
---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-  
+""" 
+@author:quincyqiang 
+@license: Apache Licence 
+@file: run.py 
+@time: 2019-05-24 01:18
+@description:
+"""
+import os
+if __name__ == '__main__':
+    for i in range(8):
+        os.system("python 02_stacking.py")
